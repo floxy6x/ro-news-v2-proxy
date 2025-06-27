e9df1c22df7b8f478c94765f5cd1f29cd06ecc1c
floxy6x <46838982+floxy6x@users.noreply.github.com>
2025-06-27 14:10:51 +0300

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,13 +1,15 @@
 export default async function handler(req, res) {
   const { url } = req.query;
-  if (!url) return res.status(400).json({ error: "Missing URL" });
+  if (!url) {
+    return res.status(400).json({ error: 'URL parameter is missing' });
+  }
 
   try {
     const response = await fetch(url);
     const data = await response.text();
-    res.setHeader("Content-Type", "application/xml");
+    res.setHeader('Content-Type', 'application/xml');
     res.status(200).send(data);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch feed" });
+    res.status(500).json({ error: 'Failed to fetch RSS feed' });
   }
 }
